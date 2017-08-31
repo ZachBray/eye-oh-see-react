2b4ee662ee04ba39ec800b95688628120810161d
Zach Bray <zachbray@googlemail.com>
2017-08-31 21:07:11 +0100

    Added missing file.

---
+++ src/typings/react-jss.d.ts
@@ -0,0 +1,19 @@
+declare module "react-jss" {
+  import * as React from "react";
+
+  export interface IStyle {
+    [key: string]: React.CSSProperties;
+  }
+
+  export type ClassNames<TStyle extends IStyle> = {
+    [P in keyof TStyle]: P
+  };
+
+  export type StylingProps<TStyle extends IStyle, TProps> = TProps & { classes: ClassNames<TStyle> };
+
+  export type ComponentNeedingStyling<TStyle extends IStyle, TProps> =
+    React.ComponentType<StylingProps<TStyle, TProps>>;
+
+  export default function injectSheet<TStyle extends IStyle, TProps>(style: TStyle)
+                  : (component: ComponentNeedingStyling<TStyle, TProps>) => React.ComponentType<TProps>;
+}
